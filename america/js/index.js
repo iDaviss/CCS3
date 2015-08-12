9c6a38d5d2e5871586e2e7ecb1ec52331469f68b
David oficina <ideas-software@live.com.mx>
2015-08-11 19:10:53 -0500

    Din del video 'Incluyendo Datos en nuestro template'

--- america/js/index.js
+++ america/js/index.js
@@ -1,10 +1,22 @@
 (function(){
 
+	var API_WEATHER_KEY="80114c7878f599621184a687fc500a12";
+	var API_WEATHER_URL="http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY + "&";
+	var IMG_WEATHER="http://openweathermap.org/img/w/";
+
+	var today = new Date();
+	var timeNow = today.toLocaleTimeString();
+
+	var cityWeather={};
+	cityWeather.zone;
+	cityWeather.icon;
+	cityWeather.temp;
+	cityWeather.temp_max;
+	cityWeather.temp_min;
+	cityWeather.main;
+
 	if(navigator.geolocation){
-
 		navigator.geolocation.getCurrentPosition(getCoords,errorFound);
-
-
 	} else{
 		alert("Por favor, actualiza tu navegador");
 	}
@@ -19,9 +31,44 @@ function errorFound(error){
 
 function getCoords(position){
 	var lat= position.coords.latitude;
-	var lan= position.coords.longitude;
-	console.log("Tu posicion es " + lat + "," + lan);
+	var lon= position.coords.longitude;
+	console.log("Tu posicion es " + lat + "," + lon);
 
+	$.getJSON(API_WEATHER_URL+"lat=" + lat + "&lon=" + lon, getCurrentWeather);
 }
+
+function getCurrentWeather(data){
+	cityWeather.zone = data.name;
+	cityWeather.icon = IMG_WEATHER + data.weather[0].icon + ".png";
+	//CONVERTIR LOS GRADOS KELVIN A CENTIGRADOS. UNICAMENTE RESTARLE 273.15
+	cityWeather.temp = data.main.temp - 273.15;
+	cityWeather.temp_max = data.main.temp_max-273.15;
+	cityWeather.temp_min = data.main.temp_min-273.15;
+	cityWeather.main = data.weather[0].main;
+
+	renderTemplate();
+};
+
+	function activateTemplate(id){
+		var t = document.querySelector(id);
+		return document.importNode(t.content, true);
+	};
+
+	function renderTemplate(){
+		var clone = activateTemplate("#template--city");
+
+		clone.querySelector("[data-time]").innerHTML= timeNow;
+		//clone.querySelector("[data-time]").innerHTML = ;
+		clone.querySelector("[data-city").innerHTML = cityWeather.zone;
+		//innerHTML se utiliza cuando una etiqueta tiene inicio y fin <></>
+		clone.querySelector("[data-icon]").src= cityWeather.icon;
+		clone.querySelector("[data-temp='max']").innerHTML=cityWeather.temp_max.toFixed(1);
+		clone.querySelector("[data-temp='min']").innerHTML=cityWeather.temp_min.toFixed(1);
+		clone.querySelector("[data-temp='current").innerHTML=cityWeather.temp.toFixed(1);
+		
+
+		$(".loader").hide();
+		$("body").append(clone);
+	}
 
 })();
